96a95a915fa843cd2c17985788b828d5a5a3a4f6
Atharv Vikhe <94232340+AtharvV2001@users.noreply.github.com>
2023-03-02 18:18:46 +0530

    added new modal

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import { Route, Routes } from 'react-router-dom';
+import { hotjar } from 'react-hotjar';
 import './App.css';
 import Home from './components/home/Home';
-import Facts from './components/facts/Facts'
+import Facts from './components/facts/Facts';
 
 function App() {
+  useEffect(()=>{
+    hotjar.initialize(3385271, 6);
+  }, [])
   return (
     <div className="App">
       <Routes>
